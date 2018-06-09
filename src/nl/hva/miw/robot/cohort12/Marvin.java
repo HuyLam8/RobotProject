dda385c09a6b3dba52a172e9eb04fb196dec4373
B. Goos <bjorn.goos@hva.nl>
2018-06-09 16:50:55 +0200

    Update van Marvin; initialiseert nu een object van LineFollower en voert vervolgens de followLine-methode van deze class uit.

--- src/nl/hva/miw/robot/cohort12/Marvin.java
+++ src/nl/hva/miw/robot/cohort12/Marvin.java
@@ -1,39 +1,40 @@
 package nl.hva.miw.robot.cohort12;
 
 import lejos.hardware.Brick;
-import lejos.hardware.Button;
-import lejos.hardware.Key;
 import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.TextLCD;
-import lejos.utility.Delay;
+import lejos.hardware.motor.*;
+import lejos.hardware.port.*;
+import lejos.hardware.sensor.*;
+import lejos.robotics.RegulatedMotor;
 
 public class Marvin {
-	
+
 	Brick brick;
-	
+	private static UnregulatedMotor motorRight = new UnregulatedMotor(MotorPort.D);
+	private static UnregulatedMotor motorLeft = new UnregulatedMotor(MotorPort.C);
+	private static RegulatedMotor motorOfHead = new EV3MediumRegulatedMotor(MotorPort.B);
+	private static UnregulatedMotor motorOfGrip = new UnregulatedMotor(MotorPort.A);
+	private static ColorSensor colorSensor = new ColorSensor(SensorPort.S4);
+	private static EV3IRSensor infraRedSensor = new EV3IRSensor(SensorPort.S1);
+  
+	final static int MODE_SIMPLE_LINE_FOLLOWER_RIGHT = 1;
+	final static int MODE_SIMPLE_LINE_FOLLOWER_LEFT = 2;
+	final static int MODE_SOMEWHAT_ADVANCED_LINE_FOLLOWER_RIGHT = 3;
+	final static int MODE_SOMEWHAT_ADVANCED_LINE_FOLLOWER_LEFT = 4;
+	final static int MODE_SOMEWHAT_ADVANCED_LINE_FOLLOWER_BORDER_UNKNOWN = 5;
+
 	public Marvin() {
 		super();
 		brick = LocalEV3.get();
 	}
-	
+
 	public static void main(String[] args) {
 		Marvin marvin = new Marvin();
 		marvin.run();
 	}
-	
+
 	private void run() {
-		TextLCD display = brick.getTextLCD();
-		display.drawString("Welkom!", 0, 3);
-		display.drawString("Team Zero", 0, 4);
-		waitForKey(Button.ENTER);
-	}
-	
-	public void waitForKey(Key key) {
-		while(key.isUp()) {
-			Delay.msDelay(100);
-		}
-		while(key.isDown()) {
-			Delay.msDelay(100);
-		}
+		LineFollower ourLineFollower = new LineFollower(motorRight, motorLeft, colorSensor);
+		ourLineFollower.followLine(MODE_SOMEWHAT_ADVANCED_LINE_FOLLOWER_BORDER_UNKNOWN);
 	}
 }
