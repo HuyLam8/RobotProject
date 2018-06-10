33db09a3f5f0c506318c10e8ebf1e673fdc1aa62
B. Goos <bjorn.goos@hva.nl>
2018-06-10 14:07:33 +0200

    added (adjustable) P-controller

--- src/nl/hva/miw/robot/cohort12/LineFollower.java
+++ src/nl/hva/miw/robot/cohort12/LineFollower.java
@@ -6,12 +6,12 @@ import lejos.hardware.motor.*;
 
 /**
  * This class facilitates a robot to follow a line by zigzagging around one of
- * its borders while moving forward. There class provides both basic and more
- * advanced versions which increase the speed and lower the likelihood of the
- * robot missing an inner curve, hence ending on the wrong side of the line.
- * Moreover, the class encompasses a line follower in which the robot
- * 'discovers' on which border of the line he starts and then follows the line
- * along this border.
+ * its borders while moving forward. This class provides several types of line
+ * followers: 1. A basic OnOff-method 2. A P-controller with the options to 2a.
+ * increase the speed/power at straight lines 2b. use a 'panic mode' when there
+ * is a risk of losing the inner curve of a bend and 2c. deal with dashed lines;
+ * 3. A 'smart' line follower which before zigzagging around the border of a
+ * line first determines whether it is placed at the right or the left border;
  * 
  * @author Bjorn Goos
  *
@@ -20,9 +20,53 @@ import lejos.hardware.motor.*;
 public class LineFollower {
 	private static ColorSensor colorSensor;
 	private static ControlDrive drive;
+
+	// The measured color values of black, white and the border. This class
+	// encompasses a 'calibrate'-method to adjust these figures.
 	private static double redColorOfBlack = 0.85;
 	private static double redColorOfWhite = 0.08;
-	private static double redColorOfBorder = 0.43;
+	private static double redColorOfBorder = (redColorOfBlack - redColorOfWhite) / 2 + redColorOfWhite;
+
+	// The power of the robot's motors in case of an on/fff-line follower
+	// If robot is placed at the right border, then OuterMoter = right motor,
+	// InnerMotor = left motor
+	// If robot is placed at the left border, then OuterMoter = left motor,
+	// InnerMotor = right motor
+	private static int onOffPowerOuterMotor = 60;
+	private static int onOffPowerInnerMotor = -15;
+
+	// The required variables for setting and adjusting the P-controller
+	// A higher kP implies that the robot will move faster to the border. This will
+	// generally lower the duration of zigzagging, but also temporarily increase the
+	// extent of the zigzagging
+	private static int kP = 250;
+	// The robot moves faster at a higher power, but the risk of losing curves also
+	// increases
+	private static int steadyPower = 30;
+	// If the robot keeps missing inner curves, then consider to increase (i.e. move
+	// closer to zero) the panic boundary. Another option is to lower the above
+	// mentioned steady and/or speed power
+	private static double panicBoundary = -0.25;
+	private static int panicPower = 100;
+	// If the robot does not accelerate soon enough when it is driving almost
+	// straight, then consider increasing the speedUpBoundary
+	private static double speedUpBoundary = 0.05;
+	private static int speedPower = 50;
+	// If the robot has to follow a dashed line as well, then set the variables
+	// below in such a way, that it will
+	private static double dashedLineColorValue = 0.75;
+	private static int dashedLinePowerOuterMotor = 10;
+	private static int dashedLinePowerInnerMotor = 30;
+	private static int steadyPowerCorrectionForDashedLine = 0;
+	private static int speedPowerCorrectionForDashedLine = 0;
+	private static int kPCorrectionForDashedLine = 0;
+	private static int speedUpBoundaryCorrectionForDashedLine = 0;
+	private static double adjustmentToPreventMissingOutsideCurve = 0;
+
+	// The required variables for the line follower that determines at which border
+	// of the line the robot is starting
+	final static int testTurnRightMotor = 60;
+	final static int testTurnLeftMotor = -15;
 
 	public LineFollower(UnregulatedMotor motorRight, UnregulatedMotor motorLeft, ColorSensor colorSensor) {
 		super();
@@ -34,8 +78,7 @@ public class LineFollower {
 	 * Make the robot follow the line in a particular way.
 	 * 
 	 * @param mode
-	 *            The mode of the LineFollower. There are five different modes which
-	 *            each make the robot follow the line in a somewhat different way.
+	 *            The mode of the LineFollower.
 	 */
 
 	public void followLine(int mode) {
@@ -48,19 +91,19 @@ public class LineFollower {
 
 		switch (mode) {
 		case 1:
-			simpleLineFollowerRightBorder();
+			OnOffRight();
 			break;
 		case 2:
-			simpleLineFollowerLeftBorder();
+			OnOffLeft();
 			break;
 		case 3:
-			somewhatMoreAdvancedLineFollowerRightBorder();
+			adjustablePControllerRight();
 			break;
 		case 4:
-			somewhatMoreAdvancedLineFollowerLeftBorder();
+			adjustablePControllerLeft();
 			break;
 		case 5:
-			somewhatMoreAdvancedLineFollowerUnkownBorder();
+			startFromUnknowBorder();
 		}
 	}
 
@@ -71,132 +114,103 @@ public class LineFollower {
 	 * going to the left when he reads a color that is more black than the surface.
 	 */
 
-	public void simpleLineFollowerRightBorder() {
+	public void OnOffRight() {
 		while (Button.ESCAPE.isUp()) {
 			drive.goForward();
 			float colorValue = colorSensor.getRed();
 			double tooWhite = colorValue - redColorOfBorder;
 			System.out.println("color: " + colorValue);
 			if (tooWhite > 0) {
-				drive.normalTurnLeft();
+				drive.setPower(onOffPowerOuterMotor, onOffPowerInnerMotor);
 			} else {
-				drive.normalTurnRight();
+				drive.setPower(onOffPowerInnerMotor, onOffPowerOuterMotor);
 			}
 		}
 	}
 
-	public void simpleLineFollowerLeftBorder() {
+	public void OnOffLeft() {
 		while (Button.ESCAPE.isUp()) {
 			float colorValue = colorSensor.getRed();
 			double tooWhite = colorValue - redColorOfBorder;
+			System.out.println("color: " + colorValue);
 			if (tooWhite > 0) {
-				drive.normalTurnRight();
+				drive.setPower(onOffPowerInnerMotor, onOffPowerOuterMotor);
 			} else {
-				drive.normalTurnLeft();
-			}
-		}
-	}
-	
-	public void pControllerWithPanicModeOnRightBorder() {
-		while (Button.ESCAPE.isUp()) {
-			int rightPower;
-			int leftPower;
-			float colorValue = colorSensor.getRed();
-			double tooWhite = colorValue - redColorOfBorder;
-			// make the 'correction' proportional on the tooWhite-error
-			if (tooWhite > -0.25) {
-				rightPower = Math.max(-100, Math.min(100, (int) (steadyPowerPercentage + kP * tooWhite)));
-				leftPower = Math.max(-100, Math.min(100, (int) (steadyPowerPercentage - kP * tooWhite)));
-				drive.setPower(rightPower, leftPower);
-			}
-			// if high risk of 'losing' the innercurve, then let the robot turn around its own axis ('panic mode')
-			else {
-				rightPower = -100;
-				leftPower = 100;
-				drive.setPower(rightPower, leftPower);
+				drive.setPower(onOffPowerOuterMotor, onOffPowerInnerMotor);
 			}
 		}
 	}
 
 	/**
-	 * In the somewhat more advanced version, there are two improvements: - the
-	 * robot zigzags less when it reads a color value close to the color value of
-	 * the border; - the robot will do more to prevent losing the inner curve
-	 * (losing the outer curve is less problematic, since the robot will then stay
-	 * on the correct side of the line and ultimately zigzag his way back to the
-	 * line)
+	 * This is a P-controller with 3 possible adjustments: 1. If there is a risk of
+	 * losing the inner curve of a bend, then there is a very strong correction.
+	 * This correction is needed since missing an inner curve means ending up on the
+	 * wrong side of the line; 2. If the robot is moving over a 'straight line',
+	 * i.e. the error is very small, then the 'speed' (i.e. the power of the
+	 * unregulated motor) increases; by this, the robot will move extra fast on
+	 * straight lines.
+	 * 
+	 * This picture shows the idea behind the first 2 adjustments:
+	 * https://imgur.com/a/3DXAoaZ (with pK= 200, goalPower = 50, speedPower = 70)
+	 * 
+	 * 3. If the robot has to follow a dashed line as well, then, with this method,
+	 * it can only do so if it *only* measures a very high tooWhite value (i.e. the
+	 * dashedLineColorValue) if it passes a dashed line. In other words, it should
+	 * be prevented that the robot measures a very high white value if it e.g.
+	 * misses the outer curve of a bend.
+	 * 
+	 * This adjusted P-controller can easily be turned into a standard P-controller
+	 * by: 1. setting the steadyPower equal to the speedPower 2.setting the
+	 * panicBoundary at e.g. -1 and 3. setting all the variables related to
+	 * following the dashed line to 0.
 	 */
-
-	public void somewhatMoreAdvancedLineFollowerRightBorder() {
+	public void adjustablePControllerRight() {
 		while (Button.ESCAPE.isUp()) {
+			int rightPower;
+			int leftPower;
 			float colorValue = colorSensor.getRed();
 			double tooWhite = colorValue - redColorOfBorder;
-
-			// If the robot is *very* close to the border,
-			// then drive straight
-			if (tooWhite < 0.02 && tooWhite > -0.02) {
-				drive.moveStraightAtHighSpeed();
+			// If there is a risk of losing the 'inner curve' of a bend, then a 'panic mode'
+			// applies
+			if (tooWhite < panicBoundary) {
+				rightPower = panicPower;
+				leftPower = -panicPower;
+				drive.setPower(rightPower, leftPower);
+			}
+			// If the robot is driving over a more or less straight line, then the speed
+			// goes up
+			else if (tooWhite < speedUpBoundary && tooWhite > -speedUpBoundary) {
+				rightPower = Math.max(-100, Math.min(100, (int) (speedPower + kP * tooWhite)));
+				leftPower = Math.max(-100, Math.min(100, (int) (speedPower - kP * tooWhite)));
+				drive.setPower(rightPower, leftPower);
 			}
 
-			// If the robot is close to the border,
-			// then let it move less back and forth and more in a straight line
-			else if (tooWhite < 0.05 && tooWhite > -0.05) {
-				if (tooWhite > 0) {
-					drive.verySmallTurnLeft();
-				} else {
-					drive.verySmallTurnRight();
-				}
-			}
+			// If the robot has to follow a dashed line as well, then it has to first detect
+			// that there is a dashed line. In our method, the robot will see (i.e. assume
+			// there is) a dashed line if the color value is above a certain threshold (i.e.
+			// the dashedLineColorValue). In this case, the robot's outer motor will
+			// actually have a somewhat lower power than it's inner motor. This way, the
+			// robot will, hopefully, continue to the next black part of the dashed line.
+			// Ideally, the robot will move something like this: https://imgur.com/a/VC9pdkQ
+			// With this method, there is ofcourse the risk that the robot will, ultimately,
+			// end up either on the wrong side of the line or will miss the line completely.
 
-			// If there is a risk of 'losing the inside of a bend',
-			// then take a sharp turn,
-			// since then the risk is too high to end up at the 'wrong' side of the line
-			else if (tooWhite < -0.2) {
-				drive.strongTurnRight();
-			}
+			// else if (tooWhite > dashedLineColorValue) {
+			// rightPower = dashedLinePowerOuterMotor;
+			// leftPower = dashedLinePowerInnerMotor;
+			// drive.setPower(rightPower, leftPower);
+			// }
 
-			// Last chance to avoid losing the bend, time for extreme measurements!
-			else if (tooWhite < -0.25) {
-				drive.extremeTurnRight();
-			}
-
-			// In all other cases, let him drive as in case of the SimpleLineFollower
+			// This is the basic P-controller
 			else {
-				if (tooWhite > 0) {
-					drive.normalTurnLeft();
-				} else {
-					drive.normalTurnRight();
-				}
+				rightPower = Math.max(-100, Math.min(100, (int) (steadyPower + kP * tooWhite)));
+				leftPower = Math.max(-100, Math.min(100, (int) (steadyPower - kP * tooWhite)));
+				drive.setPower(rightPower, leftPower);
 			}
 		}
-
 	}
 
-	public void somewhatMoreAdvancedLineFollowerLeftBorder() {
-		while (Button.ESCAPE.isUp()) {
-			float colorValue = colorSensor.getRed();
-			double tooWhite = colorValue - redColorOfBorder;
-			if (tooWhite < 0.02 && tooWhite > -0.02) {
-				drive.moveStraightAtHighSpeed();
-			} else if (tooWhite < 0.05 && tooWhite > -0.05) {
-				if (tooWhite > 0) {
-					drive.verySmallTurnRight();
-				} else {
-					drive.verySmallTurnLeft();
-				}
-			} else if (tooWhite < -0.2) {
-				drive.strongTurnLeft();
-			} else if (tooWhite < -0.25) {
-				drive.extremeTurnLeft();
-			} else {
-				if (tooWhite > 0) {
-					drive.normalTurnRight();
-				} else {
-					drive.normalTurnLeft();
-				}
-			}
-		}
-
+	public void adjustablePControllerLeft() {
 	}
 
 	/*-
@@ -209,7 +223,6 @@ public class LineFollower {
 	 * then the robot started at the right border (i.e. moved from Y to Q).
 	 * 
 	 * [w = white, b = black]
-	 * 
 	 *	 ww|bbb|ww
 	 *   ww|bbb|ww
 	 *   P | Q |
@@ -220,7 +233,7 @@ public class LineFollower {
 	 *	
 	 */
 
-	public void somewhatMoreAdvancedLineFollowerUnkownBorder() {
+	public void startFromUnknowBorder() {
 		int aantalMetingen = 0;
 		float meting1 = -1;
 		float meting13 = -1;
@@ -231,30 +244,38 @@ public class LineFollower {
 			// The first ten measurements are intended to decide where the robot is compared
 			// to the line. To do so, it is forced to move to the left and collect the
 			// required data.
-			if (aantalMetingen <= 30) {
-				drive.normalTurnLeft();
+			if (aantalMetingen <= 14) {
+				drive.setPower(testTurnRightMotor, testTurnLeftMotor);
 				if (aantalMetingen == 1) {
 					meting1 = colorValue;
 				}
-				if (aantalMetingen == 30) {
+				if (aantalMetingen == 13) {
 					meting13 = colorValue;
 				}
 			}
 			// With the collected data, it can be determined whether the robot was at the
 			// right or left border of the line, after which the robot continues its path
 			// accordingly
-			else if (aantalMetingen == 31) {
+			else {
 				System.out.println("meting 1: " + meting1);
-				System.out.println("meting 30: " + meting13);
+				System.out.println("meting 13: " + meting13);
 
 				if (meting13 > meting1) {
-					System.out.println("Start was at left.");
-					this.somewhatMoreAdvancedLineFollowerLeftBorder();
+					System.out.println("Start was at left border");
+					this.adjustablePControllerLeft();
 				} else {
-					System.out.println("Start was at right.");
-					this.somewhatMoreAdvancedLineFollowerRightBorder();
+					System.out.println("Start was at right border");
+					this.adjustablePControllerRight();
 				}
 			}
 		}
+
+	}
+
+	public void calibrate() {
+		System.out.println("Set the sensor on black");
+		LineFollower.redColorOfBlack = colorSensor.getRed();
+		System.out.println("Set the sensor on white");
+		LineFollower.redColorOfWhite = colorSensor.getRed();
 	}
 }
