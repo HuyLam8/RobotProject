c351e95ad3d2a8cdca6548e92bac87effb30b6ab
B. Goos <bjorn.goos@hva.nl>
2018-06-13 15:30:13 +0200

    Update Marvin.java

--- src/nl/hva/miw/robot/cohort12/Marvin.java
+++ src/nl/hva/miw/robot/cohort12/Marvin.java
@@ -2,38 +2,58 @@ package nl.hva.miw.robot.cohort12;
 
 import lejos.hardware.Brick;
 import lejos.hardware.Button;
-import lejos.hardware.Key;
+import lejos.hardware.Sound;
 import lejos.hardware.ev3.LocalEV3;
-import lejos.hardware.lcd.TextLCD;
-import lejos.utility.Delay;
+import lejos.hardware.motor.*;
+import lejos.hardware.port.*;
+import lejos.hardware.sensor.*;
+import lejos.robotics.RegulatedMotor;
 
 public class Marvin {
-	
+
 	Brick brick;
-	
+//	private static UnregulatedMotor motorRight = new UnregulatedMotor(MotorPort.D);
+//	private static UnregulatedMotor motorLeft = new UnregulatedMotor(MotorPort.C);
+	private static RegulatedMotor motorRight = new EV3LargeRegulatedMotor(MotorPort.D);
+	private static RegulatedMotor motorLeft = new EV3LargeRegulatedMotor(MotorPort.C);
+	private static RegulatedMotor motorOfHead = new EV3MediumRegulatedMotor(MotorPort.B);
+	private static UnregulatedMotor motorOfGrip = new UnregulatedMotor(MotorPort.A);
+	//private static ColorSensor colorSensor = new ColorSensor(SensorPort.S4);
+	private static EV3IRSensor infraRedSensor = new EV3IRSensor(SensorPort.S1);
+	private static EV3TouchSensor touchSensor = new EV3TouchSensor(SensorPort.S2);
+
+	// The constants below are for setting the right mode of a LineFollower
+	final static int MODE_ONOFF_FOLLOWER_RIGHT = 1;
+	final static int MODE_ONOFF_FOLLOWER_LEFT = 2;
+	final static int MODE_ADJUSTED_P_CONTROLLER_RIGHT = 3;
+	final static int MODE_ADJUSTED_P_CONTROLLER_LEFT = 4;
+	final static int MODE_UNKNOWN_STARTING_POINT = 5;
+
 	public Marvin() {
 		super();
 		brick = LocalEV3.get();
 	}
-	
+
 	public static void main(String[] args) {
 		Marvin marvin = new Marvin();
 		marvin.run();
 	}
-	
+
 	private void run() {
-		TextLCD display = brick.getTextLCD();
-		display.drawString("Welkom!", 0, 3);
-		display.drawString("Team Zero", 0, 4);
-		waitForKey(Button.ENTER);
-	}
-	
-	public void waitForKey(Key key) {
-		while(key.isUp()) {
-			Delay.msDelay(100);
-		}
-		while(key.isDown()) {
-			Delay.msDelay(100);
-		}
+
+//		 LineFollower ourLineFollower = new LineFollower(motorRight, motorLeft,
+//		 colorSensor); 
+//		 ourLineFollower.followLine(MODE_ADJUSTED_P_CONTROLLER_RIGHT);
+
+		// ObjectAvoider ourObjectAvoider = new ObjectAvoider(motorRight, motorLeft,
+		// motorOfHead, motorOfGrip,
+		// infraRedSensor);
+		// ourObjectAvoider.startObjectAvoider(); 
+
+		AvoiderDieWerkt ourObjectAvoider = new AvoiderDieWerkt(motorRight, motorLeft, motorOfHead, motorOfGrip,
+				infraRedSensor, touchSensor);
+//		ourObjectAvoider.run();
+//		ourObjectAvoider.playWithMarvin(15000);
+		ourObjectAvoider.walkThroughLabyrinth();
 	}
 }
