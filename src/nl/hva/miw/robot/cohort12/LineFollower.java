e457744a78ffcc9efabf254e0fe62d7b4c50b6ee
B. Goos <bjorn.goos@hva.nl>
2018-06-14 09:55:44 +0200

    Update LineFollower.java

--- src/nl/hva/miw/robot/cohort12/LineFollower.java
+++ src/nl/hva/miw/robot/cohort12/LineFollower.java
@@ -8,12 +8,10 @@ import lejos.hardware.motor.*;
  * This class facilitates a robot to follow a line by zigzagging around one of
  * its borders while moving forward. This class provides several types of line
  * followers: 1. A basic OnOff-method 2. A P-controller with the options to 2a.
- * increase the speed/power at straight lines or, more generally, 2b. make the
- * target speed of the robot a function of the difference between the current
- * and the last error, 2c. use a 'panic mode' when there is a risk of losing the
- * inner curve of a bend and 2d. deal with dashed lines; 3. A 'smart' line
- * follower which before zigzagging around the border of a line first determines
- * whether it is placed at the right or the left border.
+ * increase the speed/power at straight lines 2b. use a 'panic mode' when there
+ * is a risk of losing the inner curve of a bend and 2c. deal with dashed lines;
+ * 3. A 'smart' line follower which before zigzagging around the border of a
+ * line first determines whether it is placed at the right or the left border;
  * 
  * @author Bjorn Goos
  *
@@ -43,15 +41,15 @@ public class LineFollower {
 	// A higher kP implies that the robot will move faster to the border of the
 	// line. This will generally lower the duration of zigzagging, but also
 	// temporarily increase the extent of the zigzagging
-	private static int kP = 100; // 100
+	private static int kP = 150;
 	// The robot moves faster at a higher power, but the risk of losing curves also
 	// increases
-	private static int steadyPower = 15; // 15
+	private static int steadyPower = 37;
 	// If the robot keeps missing inner curves, then consider to increase (i.e. move
 	// closer to zero) the panic boundary. Another option is to lower the above
-	// mentioned steady power n n
+	// mentioned steady power
 	static boolean panicMode = true;
-	private static double panicBoundary = -0.20;
+	private static double panicBoundary = -0.25;
 	private static int panicKpInner = 1000;
 	private static int panicSteadyPowerInner = 250;
 	private static int panicKpOuter = -1000;
@@ -59,28 +57,17 @@ public class LineFollower {
 	private static int panicPower = 100;
 	// If the robot does not accelerate soon enough when it is driving almost
 	// straight, then consider increasing the speedUpBoundary
-	static boolean speeder = false;
-	private static double speedUpBoundary = 0.10;
-	private static int speedPower = 60;
-	private static int speedKp1 = 600;
-	private static int speedKp2 = 315;
+	static boolean useSpeeder = false;
+	private static double speedUpBoundary = 0.05;
+	private static int speedPower = 50;
+	private static int speedKp1 = 400;
+	private static int speedKp2 = 100;
 	// If the robot has to follow a dashed line as well, then use these variables
-	private static boolean dashedLines = false;
+	static boolean dealWithDashedLines = false;
 	private static double dashedLineHelperBoundary = 0.25;
 	private static double dashedLineBoundary = 0.35;
 	private static int dashedLinePowerOuterMotor = 20;
 	private static int dashedLinePowerInnerMotor = 30;
-	// With a standard P-controller, the total power (defined as the sum of the
-	// power of the right and the left motor) is constant. Using the
-	// variableSpeed-mode, makes the total power a function of the difference
-	// between the current and the last error. This way, the speed will be higher at
-	// straight lines and lower at curves.
-	private static boolean variableSpeed = true;
-	private static double lastColorValue = redColorOfBorder;
-	private static double colorValueVariableSpeedThreshold = 0.03; // test
-	private static int minimumPower = 20;
-	private static int maximumPower = 90;
-	private static double speedChangeFraction = 0.015;
 
 	// The required variables for the line follower that determines at which border
 	// of the line the robot is starting
@@ -107,7 +94,6 @@ public class LineFollower {
 		Sound.beepSequenceUp();
 		System.out.println("Press any key to start the Line Follower");
 		Button.waitForAnyPress();
-		drive.goForward();
 
 		switch (mode) {
 		case 1:
@@ -164,14 +150,14 @@ public class LineFollower {
 	/**
 	 * This is a P-controller with 3 possible adjustments: 1. If there is a risk of
 	 * losing the inner curve of a bend, then there is a very strong correction.
-	 * This correction is needed for sharp curves since missing an inner curve means
-	 * ending up on the wrong side of the line; 2. If the robot is moving over a
-	 * 'straight line', i.e. the error is very small, then the 'speed' (i.e. the
-	 * power of the unregulated motors) increases; by this, the robot will move
-	 * extra fast on straight lines.
+	 * This correction is needed since missing an inner curve means ending up on the
+	 * wrong side of the line; 2. If the robot is moving over a 'straight line',
+	 * i.e. the error is very small, then the 'speed' (i.e. the power of the
+	 * unregulated motors) increases; by this, the robot will move extra fast on
+	 * straight lines.
 	 * 
-	 * The dotted lines in this picture show the idea behind the first two
-	 * adjustments: https://imgur.com/a/5cO37Oc
+	 * This picture shows the idea behind the first 2 adjustments:
+	 * https://imgur.com/a/3DXAoaZ (with pK= 200, goalPower = 50, speedPower = 70)
 	 * 
 	 * 3. If the robot has to follow a dashed line as well, then, with this method,
 	 * it can only do so if it *only* measures a very high tooWhite value (i.e. the
@@ -180,7 +166,7 @@ public class LineFollower {
 	 * misses the outer curve of a bend.
 	 * 
 	 * This adjusted P-controller can easily be turned into a standard P-controller
-	 * by setting all the booleans (panicMode, speeder, dashedLines) at false.
+	 * by setting all the booleans (useSpeeder, dealWithDashedLines) at false.
 	 */
 	private void adjustablePControllerRight() {
 		while (Button.ESCAPE.isUp()) {
@@ -192,10 +178,12 @@ public class LineFollower {
 			// applies
 			if (panicMode == true) {
 				if (tooWhite < panicBoundary) {
-					// rightPower = Math.max(-100, Math.min(100, (int) (panicSteadyPowerInner +
-					// panicKpInner * tooWhite)));
-					// leftPower = Math.max(-100, Math.min(100, (int) (panicSteadyPowerOuter -
-					// panicKpOuter * tooWhite)));
+					// rightPower = Math.max(-100, Math.min(100, (int) (panicSteadyPowerOuter +
+					// panicKpOuter *
+					// tooWhite)));
+					// leftPower = Math.max(-100, Math.min(100, (int) (panicSteadyPowerInner -
+					// panicKpInner *
+					// tooWhite)));
 					rightPower = -panicPower;
 					leftPower = panicPower;
 					drive.setPower(rightPower, leftPower);
@@ -203,16 +191,16 @@ public class LineFollower {
 			}
 			// If the robot is driving over a more or less straight line, then the speed
 			// goes up
-			if (speeder == true) {
-				if (tooWhite <= speedUpBoundary && tooWhite >= -speedUpBoundary) {
-					if (tooWhite >= -speedUpBoundary && tooWhite <= 0) {
+			if (useSpeeder == true) {
+				if (tooWhite < speedUpBoundary && tooWhite > -speedUpBoundary) {
+					if (tooWhite > -speedUpBoundary) {
 						rightPower = Math.max(-100, (Math.min(100, (int) (speedPower + speedKp1 * tooWhite))));
-						leftPower = Math.max(-100, (Math.min(100, (int) (speedPower + speedKp2 * tooWhite))));
+						leftPower = Math.max(-100, Math.min(100, (int) (speedPower - speedKp1 * tooWhite)));
 						drive.setPower(rightPower, leftPower);
 					}
-					if (tooWhite <= speedUpBoundary && tooWhite > 0) {
-						rightPower = Math.max(-100, (Math.min(100, (int) (speedPower - speedKp2 * tooWhite))));
-						leftPower = Math.max(-100, (Math.min(100, (int) (speedPower - speedKp1 * tooWhite))));
+					if (tooWhite < speedUpBoundary) {
+						rightPower = Math.max(-100, (Math.min(100, (int) (speedPower + speedKp2 * tooWhite))));
+						leftPower = Math.max(-100, Math.min(100, (int) (speedPower - speedKp2 * tooWhite)));
 						drive.setPower(rightPower, leftPower);
 					}
 				}
@@ -228,7 +216,7 @@ public class LineFollower {
 			// ultimately, end up either on the wrong side of the line or miss the line
 			// completely.
 
-			if (dashedLines == true) {
+			if (dealWithDashedLines == true) {
 				// Make sure that the robot never reaches a 'very white' surface when e.g.
 				// missing the outer curve of a bend
 				if (tooWhite > dashedLineHelperBoundary) {
@@ -244,17 +232,8 @@ public class LineFollower {
 				}
 			}
 
-			// This is the basic P-controller with a possibility to adapt the speed to the
-			// amount of change in the speed
+			// This is the basic P-controller
 			else {
-				if (variableSpeed == true) {
-					steadyPower = (int) ((1 - speedChangeFraction) * steadyPower + speedChangeFraction * maximumPower);
-					if (Math.abs(colorValue - lastColorValue) > colorValueVariableSpeedThreshold) {
-						steadyPower = minimumPower;
-					}
-					lastColorValue = colorValue;
-				}
-				// This is the basic P-controller
 				rightPower = Math.max(-100, Math.min(100, (int) (steadyPower + kP * tooWhite)));
 				leftPower = Math.max(-100, Math.min(100, (int) (steadyPower - kP * tooWhite)));
 				drive.setPower(rightPower, leftPower);
@@ -265,24 +244,25 @@ public class LineFollower {
 	private void adjustablePControllerLeft() {
 	}
 
-	/**
-	 * This method first lets the robot determine on which side of the line it is.
-	 * This is done by forcing the robot to drive to the left for a short time.
-	 * Then, the color value of the first measurement is compared to the value of
-	 * the 10th measurement. If there is an increase in the color value, then the
-	 * robot has started at the left border of the line, i.e. moved from X to P in
-	 * the drawing below. On contrary, if there is an decrease in the color value,
-	 * then the robot started at the right border (i.e. moved from Y to Q). The
-	 * robot then uses this information to either follow the left or right border.
-	 */
 	/*-
-	[w = white, b = black]
-	  ww|bbb|ww
-	  ww|bbb|ww
-	  P | Q |
-	   \|  \|
-	    X   Y	
-	*/
+	 * This method first lets the robot determine on which side of the line it is. 
+	 * This is done by forcing the robot to drive to the left for a short time. 
+	 * Then, the color value of the first measurement is compared to the value of 
+	 * the 10th measurement. If there is an increase in the color value, then the 
+	 * robot has started at the left border of the line, i.e. moved from X to P in
+	 * the picture below. On contrary, if there is an decrease in the color value, 
+	 * then the robot started at the right border (i.e. moved from Y to Q).
+	 * 
+	 * [w = white, b = black]
+	 *	 ww|bbb|ww
+	 *   ww|bbb|ww
+	 *   P | Q |
+	 *    \|  \|
+	 *     X   Y
+	 *     
+	 * The robot then uses this information to either follow the left or right border.
+	 *	
+	 */
 
 	private void startFromUnknowBorder() {
 		int aantalMetingen = 0;
@@ -323,9 +303,6 @@ public class LineFollower {
 
 	}
 
-	/**
-	 * This method can be used to adjust the color values of black and white.
-	 */
 	public void calibrate() {
 		System.out.println("Put the sensor on black");
 		Sound.beepSequenceUp();
@@ -338,37 +315,4 @@ public class LineFollower {
 		LineFollower.redColorOfWhite = colorSensor.getRed();
 		System.out.println("White: " + redColorOfWhite);
 	}
-
-	public void useSpeeder(boolean on) {
-		speeder = on;
-	}
-
-	public void useVariableSpeed(boolean on) {
-		variableSpeed = on;
-	}
-
-	public void usePanicMode(boolean on) {
-		panicMode = on;
-	}
-
-	public void useDashedLines(boolean on) {
-		dashedLines = on;
-	}
-
-	public boolean isSpeederUsed() {
-		return speeder;
-	}
-
-	public boolean isVariableSpeedUsed() {
-		return variableSpeed;
-	}
-
-	public boolean isPanicModeUsed() {
-		return panicMode;
-	}
-
-	public boolean isDashedLinesUsed() {
-		return dashedLines;
-	}
-
 }
